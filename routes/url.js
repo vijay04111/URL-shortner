808dd08c8eb7fbc9b5ab40ba2239aec486b521ff
vijay04111 <vijaykhot2170@gmail.com>
2026-01-05 14:57:27 +0530

    first

--- routes/url.js
+++ routes/url.js
@@ -7,14 +7,20 @@ router.post('/', async (req, res) => {
   const body = req.body;
 
    let genrateID= shortid.generate()
-
   await URL.create({
     shortId:genrateID,
     redirectURL:body.redirectURL,
     visitHistory: []
   });
-
   res.redirect('/');
 });
+
+
+router.delete('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId;
+  await URL.findOneAndDelete({ shortId });
+  return res.json({ msg: "Data deleted" });
+});
+
 
 module.exports = router;
